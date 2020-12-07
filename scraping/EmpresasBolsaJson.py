df3a3ef2125473d858cb19cf56b87550659e853d
xXaVeRaXx <avdepaul11@gmail.com>
2020-12-07 17:14:11 +0100

    He Trabajado pero no he trabajado

--- scraping/EmpresasBolsaJson.py
+++ scraping/EmpresasBolsaJson.py
@@ -8,6 +8,8 @@ soup = BeautifulSoup(page, "html5lib")
 tabla = soup.find('table', attrs={'id': 'tb1_1139'})#id tabla
 tabla_WhereTr = soup.find('tbody')
 
+query = "INSERT INTO Indexes (name, value, variation) VALUES "
+
 json=list()
 name=""
 price=""
@@ -22,14 +24,27 @@ for fila in tabla_WhereTr.find_all("tr"):
             print("Indice:", name)
         if nroCelda==2:
             price=celda.text
+            price = price.replace(".", "")
+            price = price.replace(",", ".")
             print("Valor:", price)
         if nroCelda==3:
             change=celda.text
+            change = change.replace(",", ".")
+            change = change.rstrip("%")
             print("%Change:", change)
         nroCelda=nroCelda+1
-    json.append({
-        'Nombre': name,
-        'Precio': price,
-        'Porcentaje': change
-    })
-print(json)        
+    
+    
+    if name and price:
+        try:
+            query = query + "('" +name+ "', " +price+ ", " +change+ "), "
+            json.append({
+                'Nombre': name,
+                'Precio': price,
+                'Porcentaje': change
+            })
+        except ValueError:
+            pass
+print(json) 
+query = query + ";"
+print(query) 
