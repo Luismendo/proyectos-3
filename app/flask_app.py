243a0c126f3e0604cc8863a449d4026a9c058bd6
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2021-02-10 21:14:51 +0100

    Because WSGI

--- app/flask_app.py
+++ app/flask_app.py
@@ -6,6 +6,16 @@ from database import db, User
 from flask import (g, session)
 
 
+db.init_app(app)
+bcrypt.init_app(app)
+
+app.register_blueprint(blueprints.auth)
+app.register_blueprint(blueprints.indexes)
+app.register_blueprint(blueprints.marketstack)
+app.register_blueprint(blueprints.noticias)
+app.register_blueprint(blueprints.opiniones)
+
+
 @app.before_request
 def before_request():
     if 'user_id' in session:
@@ -13,15 +23,5 @@ def before_request():
     else:
         g.user = None
 
-
 if __name__ == '__main__':
-    db.init_app(app)
-    bcrypt.init_app(app)
-
-    app.register_blueprint(blueprints.auth)
-    app.register_blueprint(blueprints.indexes)
-    app.register_blueprint(blueprints.marketstack)
-    app.register_blueprint(blueprints.noticias)
-    app.register_blueprint(blueprints.opiniones)
-
     app.run(debug=config.DEBUG)
