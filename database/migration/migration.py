a0e2b4bbbc472e3be27fd1ba4de5e845fa417393
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2021-02-23 00:56:37 +0100

    Migration script

--- database/migration/migration.py
+++ database/migration/migration.py
@@ -1,45 +1,36 @@
 import pymysql
 import config
 
-conn = pymysql.connect(host=config.HOST,
-                       db=config.DB_DATABASE,
-                       user=config.DB_USER,
-                       password=config.DB_PASS,
-                       charset='utf8mb4',
-                       cursorclass=pymysql.cursors.DictCursor)
+old_conn = pymysql.connect(host=config.DB_HOST,
+                           db='proyectos3',
+                           user=config.DB_USER,
+                           password=config.DB_PASS,
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor)
 
-with conn.cursor() as cur:
-    cur.execute("SELECT DISTINCT(name) FROM indexes_old")
-    idxs = [(idx['name'],) for idx in cur.fetchall()]
-    cur.executemany("INSERT INTO indexes(name) VALUES(%s)", idxs)
+new_conn = pymysql.connect(host=config.DB_HOST,
+                           db='proyectos3_test',
+                           user=config.DB_USER,
+                           password=config.DB_PASS,
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor)
 
-    cur.execute("SELECT * FROM indexes")
-    all_idxs = {idx['name']: idx['id'] for idx in cur.fetchall()}
+with old_conn.cursor() as old_cur:
+    with new_conn.cursor() as new_cur:
+        old_cur.execute("SELECT DISTINCT(name) FROM Indexes")
+        idxs = [(idx['name'],) for idx in old_cur.fetchall()]
 
-    cur.execute("SELECT * FROM indexes_old")
-    old_idxs = cur.fetchall()
+        new_cur.executemany("INSERT INTO indexes(name) VALUES(%s)", idxs)
+        new_cur.execute("SELECT * FROM indexes")
+        all_idxs = {idx['name']: idx['id'] for idx in new_cur.fetchall()}
 
-    new_idxs = list()
-    for idx in old_idxs:
-        new_idxs.append((all_idxs[idx['name']], idx['value'], idx['variation'], idx['date']))
+        old_cur.execute("SELECT * FROM Indexes")
+        new_values = [(all_idxs[idx['name']], idx['value'], idx['variation'], idx['date']) for idx in old_cur.fetchall()]
+        if new_values:
+            new_cur.executemany("""INSERT INTO `values`(index_id, value, variation, timestamp)
+                                   VALUES (%s, %s, %s, %s)
+                                """, new_values)
+            new_conn.commit()
 
-    if new_idxs:
-        cur.executemany("""INSERT INTO data(index_id, value, variation, timestamp)
-                           VALUES (%s, %s, %s, %s)
-                        """, new_idxs)
-
-    cur.execute("SELECT id, company FROM opinions")
-    opinions = cur.fetchall()
-
-    opinion_updates = list()
-    for opinion in opinions:
-        opinion_updates.append((all_idxs[opinion['company']], opinion['id']))
-
-    if opinion_updates:
-        cur.executemany("""UPDATE opinions SET index_id = %s
-                           WHERE id = %s
-                        """, opinion_updates)
-
-    conn.commit()
-
-conn.close()
+old_conn.close()
+new_conn.close()
